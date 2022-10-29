a1cee69fd274a560c3bfd19b3eef72b2ab7300ce
Manasseh <hirekaanm3@gmail.com>
2022-10-29 05:01:25 +0100

    Update Add Components to Home Page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Button from '../Components/Button'
+import Footer from '../Components/Footer'
+import Header from '../Components/Header'
 
 function Home() {
   return (
-    <>
-    <h1>Hello HomePage Test</h1>
-    </>
+    <div style={{ maxWidth:'960px', padding:'15px', margin:'auto'}}>
+        
+            <Header/>
+            <Button title='Twitter Link' link=''/>
+            <Button title='Zuri Team' link='https://training.zuri.team/' id='btn__zuri'/>
+            <Button title='Zuri Books' link='http://books.zuri.team' id='books'/>
+            <Button title='Python Books' link='https://books.zuri.team/' id='book__python'/>
+            <Button title='Background Check for Coders' link='https://background.zuri.team/' id='pitch'/>
+            <Button title='Design Books' link='https://books.zuri.team/design-rules' id='book__design'/>
+            <Footer/>
+        
+    </div>
   )
 }
 
