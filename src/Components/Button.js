aefd3ab1aa91407b96ab80c69686471164a31e69
Manasseh <hirekaanm3@gmail.com>
2022-11-04 15:06:44 +0100

    style: Add target attribute to external link tags to allow opening of external link in a window new tab

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Button({title, link, id}) {
+function Button({title, link, id, externalLink}) {
   return (
     <div style={{backgroundColor:'#EAECF0', textAlign:'center', padding:'24px 32px', borderRadius:'8px', marginTop:'8px'}}>
-        <a id={id} href={link} style={linkStyle}>{title}</a></div>
+        {externalLink ? <a id={id} href={link} style={linkStyle} target='_blank'>{title}</a> : <Link to={link} style={linkStyle}>{title}</Link>}</div>
   )
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,12 +8,12 @@ function Home() {
     <div style={{ maxWidth:'960px', padding:'15px', margin:'auto'}}>
         
             <Header/>
-            <Button title='Twitter Link' link=''/>
-            <Button title='Zuri Team' link='https://training.zuri.team/' id='btn__zuri'/>
-            <Button title='Zuri Books' link='http://books.zuri.team' id='books'/>
-            <Button title='Python Books' link='https://books.zuri.team/' id='book__python'/>
-            <Button title='Background Check for Coders' link='https://background.zuri.team/' id='pitch'/>
-            <Button title='Design Books' link='https://books.zuri.team/design-rules' id='book__design'/>
+            <Button title='Twitter Link' link='' externalLink = {true} />
+            <Button title='Zuri Team' link='https://training.zuri.team/' id='btn__zuri' externalLink = {true} />
+            <Button title='Zuri Books' link='http://books.zuri.team' id='books' externalLink = {true} />
+            <Button title='Python Books' link='https://books.zuri.team/' id='book__python' externalLink = {true} />
+            <Button title='Background Check for Coders' link='https://background.zuri.team/' id='pitch' externalLink = {true} />
+            <Button title='Design Books' link='https://books.zuri.team/design-rules' id='book__design' externalLink = {true} />
             <Button title='Contact Me' link='/contact' id='contact'/>
             <Footer/>
         
