529df2ed19657b6f20352a8d2e3d2c7da981142b
Manasseh <hirekaanm3@gmail.com>
2022-11-05 18:41:24 +0100

    Style: Changed the profile image to my image and the twitter name

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 
-import img from '../images/profile__img.png'
+import img from '../images/manasseh.jpg'
 import { useLocation } from 'react-router-dom'
 
 function Header() {
@@ -9,7 +9,7 @@ function Header() {
     <>
     {location.pathname === '/' && <div style={{textAlign:'center'}}>
         <img id='profile_img' src={img} alt="" style={{height:100, borderRadius:50, objectFit:'cover'}} />
-        <p id='twitter'>Annete Black</p>
+        <p id='twitter'>@Awsome_ideas</p>
         <p id='slack' hidden>Slack username</p>
     </div>}
     {location.pathname === '/contact' && <h1 style={{padding:'10px', paddingBottom:'0px', fontSize:'36px', fontWeight:'600', color:'#101828'}}>Contact Me</h1>}
