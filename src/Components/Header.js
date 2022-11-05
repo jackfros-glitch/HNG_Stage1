49f7f94aefc7c9b002c96397ab8f22d7ab5a1496
Manasseh <hirekaanm3@gmail.com>
2022-11-05 22:15:12 +0100

    Style: Rename the html tags in the Header Component to be more descriptive and to improve SEO of the webapp

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,14 +6,14 @@ function Header() {
   const location = useLocation()
 
   return (
-    <>
+    <header>
     {location.pathname === '/' && <div style={{textAlign:'center'}}>
         <img id='profile_img' src={img} alt="" style={{height:100, borderRadius:50, objectFit:'cover'}} />
         <p id='twitter'>@Awsome_ideas</p>
         <p id='slack' hidden>Slack username</p>
     </div>}
     {location.pathname === '/contact' && <h1 style={{padding:'10px', paddingBottom:'0px', fontSize:'36px', fontWeight:'600', color:'#101828'}}>Contact Me</h1>}
-    </>
+    </header>
     
   )
 }
