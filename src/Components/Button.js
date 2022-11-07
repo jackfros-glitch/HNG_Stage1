b0af482d89c1dfbcbf319520963cc3e6e7c068a9
Manasseh <hirekaanm3@gmail.com>
2022-11-07 23:55:58 +0100

    Style: Add id attribute to the contact link on the home page

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Button({title, link, id, externalLink}) {
   return (
     <div style={{backgroundColor:'#EAECF0', textAlign:'center', padding:'24px 32px', borderRadius:'8px', marginTop:'8px'}}>
-        {externalLink ? <a id={id} href={link} style={linkStyle} target='_blank'>{title}</a> : <Link to={link} style={linkStyle}>{title}</Link>}</div>
+        {externalLink ? <a id={id} href={link} style={linkStyle} target='_blank'>{title}</a> : <Link to={link} id={id} style={linkStyle}>{title}</Link>}</div>
   )
 }
 
